d0db95e97fb1e883ed6dce90222ecf8964759021
bigwheel <k.bigwheel+eng@gmail.com>
2017-02-22 09:42:52 +0900

    Testing

--- build.sbt
+++ build.sbt
@@ -4,4 +4,4 @@ version := "1.0"
 
 scalaVersion := "2.12.1"
 
-libraryDependencies += "com.github.bigwheel" %% "thpy" % "0.1.0"
+libraryDependencies += "com.github.bigwheel" %% "thpy" % "0.1.0-SNAPSHOT"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,12 @@
+import com.github.bigwheel.thpy.break
+import com.github.bigwheel.thpy.Macro.anyToTyped
+
 object Main {
 
   def main(args: Array[String]): Unit = {
-    println("hello")
+    val a = 10
+    break(a)
+    println("Welcome to Thpy at Main.main(/home/kbigwheel/code/thpy-sample/src/main/scala/Main.scala:10)")
   }
 
 }
